85973f152185f49bac9fe0453ca54a18af34cf21
Laura Strehlow <laurastrehlow09@aol.com>
2021-01-20 17:24:11 +0100

    Add storybook

---
+++ components/app-grid.stories.js
@@ -0,0 +1,10 @@
+import '../styles/index.css'
+
+import './app-grid.css'
+import appGrid from './app-grid.html'
+
+export default {
+  title: 'app-grid',
+}
+
+export const base = () => appGrid
---
+++ components/quiz-card.stories.js
@@ -0,0 +1,10 @@
+import '../styles/index.css'
+
+import './quiz-card.css'
+import quizCard from './quiz-card.html'
+
+export default {
+  title: 'quiz-card',
+}
+
+export const base = () => quizCard
